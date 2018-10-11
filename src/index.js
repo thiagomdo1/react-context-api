7dca323b717858b29b5c3026b1bd40c545a731c8
Thiago Oliveira <thiagomdo1@gmail.com>
2018-10-10 21:15:20 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component, createContext } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const style = {
+  padding: "20px",
+  color: "#fff",
+  fontWeight: 700
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppContext = createContext();
+
+class AppProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+    console.log(props);
+  }
+
+  handleChange(e) {
+    this.setState({ [`${e.target.name}`]: e.target.value });
+  }
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        <input
+          type="number"
+          name="number"
+          value={this.state.number}
+          onChange={e => this.handleChange(e)}
+        />
+        <hr />
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
+
+const Blue = ({ number }) => {
+  return (
+    <div style={{ ...style, backgroundColor: "blue" }}>
+      <AppContext.Consumer>{context => context.number}</AppContext.Consumer>
+    </div>
+  );
+};
+
+const Green = ({ number }) => {
+  return (
+    <div style={{ ...style, backgroundColor: "green" }}>
+      <Blue number={number} />
+    </div>
+  );
+};
+
+class Red extends Component {
+  render() {
+    return (
+      <AppProvider>
+        <div style={{ ...style, backgroundColor: "red" }}>
+          <AppContext.Consumer>{context => context.number}</AppContext.Consumer>
+          <Green />
+        </div>
+      </AppProvider>
+    );
+  }
+}
+
+ReactDOM.render(<Red />, document.getElementById("root"));
